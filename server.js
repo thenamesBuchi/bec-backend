93805319c489e13f8871e2549c6ddc254ac54994
thenamesBuchi <lesleiibeh5@gmail.com>
2025-11-29 13:17:51 +0400

    `Added require for path module and updated line count`

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-
+const path = require("path");
 const app = express();
 
 // Middleware
